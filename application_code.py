24fdeb9c0fedd0b1edef1d5d4391f8cca43cd2a1
Shuaiyi Bu <15843126572@163.com>
2019-05-02 19:36:55 -0400

    Update application_code.py

--- application_code.py
+++ application_code.py
@@ -66,8 +66,8 @@ data = {
     val new_user_recommendations_rating_title_and_count_RDD = new_user_recommendations_rating_RDD.join(large_movies_title).join(movie_rating_counts_RDD);
     val recommendations_RDD = new_user_recommendations_rating_title_and_count_RDD.map(line =>  (line._2._1._2, line._2._1._1, line._2._2));
     val top_movies = recommendations_RDD.filter(line => line._3 >= 25).takeOrdered(25)(Ordering[Double].reverse.on(x => x._2));
-val new_r = sc.parallelize(top_movies);
-new_r.saveAsTextFile("/user/sb6606/top_movies");
+    val new_r = sc.parallelize(top_movies);
+    new_r.saveAsTextFile("/user/sb6606/top_movies");
 """)
 }
 
